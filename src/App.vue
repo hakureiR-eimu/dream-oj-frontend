8aeaec89170e65e5cae2a02313e95f6333b18853
CodeOfUsami <zhang0285@foxmail.com>
2025-02-12 23:20:34 +0800

    feat:add element-plus

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,16 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+// const val = ref(0)
+// const increment = () => {
+//   val.value++
+// }
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+    <!-- <el-button @click="increment">Default</el-button>
+    <div>{{ val }}</div> -->
   </header>
-
-  <RouterView />
 </template>
 
 <style scoped>
